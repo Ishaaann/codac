371848da45c08955050b929b2bc1ca695cd621e6
Ishaan Raj <78454868+Ishaaann@users.noreply.github.com>
2026-07-03 22:27:45 +0530

    server

---
+++ src/connections/redis.js
@@ -0,0 +1,5 @@
+ function connectToRedis() {
+
+ }
+
+ export { connectToRedis };
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import http from 'http';
+import express from 'express';
+import { setUpWebSocketServer } from './websockets/wss.js';
+import { connectToRedis } from './connections/redis.js';
+
+const app = express();
+const port  = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.status(200).send({ status: 'OK' , message: 'Server is running' });
+});
+
+const server = http.createServer(app);
+
+setUpWebSocketServer(server);
+
+async function startServer(){
+    try{
+        await connectToRedis();
+        server.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+        process.exit(1);    
+    }
+}
+
+startServer();
